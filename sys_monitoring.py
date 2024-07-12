3a6f88753d0f603e9b9b00e374bdeb17321496e3
Redhgar <petrenyi.peti@gmail.com>
2024-07-12 23:51:38 +0200

    monitoring part is working

--- sys_monitoring.py
+++ sys_monitoring.py
@@ -2,9 +2,35 @@ import psutil
 import time
 import logging
 
-#logging alap beállítások
+#logging base settings
 
 logging.basicConfig(filename='system_monitoring.log', 
 			level=logging.INFO, 
 			format='%(asctime)s - %(levelname)s - %(message)s')
 
+def log_system_usage():
+	while True:
+		# cpu usage
+		cpu_percent = psutil.cpu_percent(interval=1)
+		
+		# memory usage
+		memory = psutil.virtual_memory()
+
+		# disk usage
+		disk = psutil.disk_usage('/')
+
+		# network traffic
+		net = psutil.net_io_counters()
+
+
+		#logging
+		logging.info(f'CPU usage: {cpu_percent}%')
+		logging.info(f'Memory usage: {memory.percent}%')
+		logging.info(f'Disk usage: {disk.percent}%')
+		logging.info(f'Network incoming traffic: {net.bytes_recv}')
+
+		# logtime
+		time.sleep(60)
+
+if __name__== '__main__':
+	log_system_usage()
