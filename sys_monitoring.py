05540bb33032b855fff3dc086301ac60f17815be
Redhgar <petrenyi.peti@gmail.com>
2024-07-16 05:48:46 +0200

    email sending is working

--- sys_monitoring.py
+++ sys_monitoring.py
@@ -3,12 +3,34 @@ import time
 import logging
 import smtplib
 from email.mime.multipart import MIMEMultipart
-from email.mime.test import MIMEText
-#logging base settings
+from email.mime.text import MIMEText
 
-logging.basicConfig(filename='system_monitoring.log', 
-			level=logging.INFO, 
-			format='%(asctime)s - %(levelname)s - %(message)s')
+
+#logging base settings
+logging.basicConfig(filename='system_monitoring.log',  #name of the logging file
+			level=logging.INFO,		#level of logging 
+			format='%(asctime)s - %(levelname)s - %(message)s') #logging format
+
+#email settings
+def send_email(subject, body):
+	from_email =
+	to_email =
+	password = 
+
+	#E-mail creation
+	msg = MIMEMultipart()
+	msg['From'] = from_email
+	msg['To'] = to_email
+	msg['Subject'] = subject
+
+	msg.attach(MIMEText(body, 'plain'))
+
+	#SMTP server settings and email sending
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.starttls()
+	server.login(from_email, password)
+	server.sendmail(from_email, to_email, msg.as_string())
+	server.quit()
 
 def log_system_usage():
 	while True:
@@ -24,15 +46,24 @@ def log_system_usage():
 		# network traffic
 		net = psutil.net_io_counters()
 
+		
 
+		#logging message
+		log_message = (
+			f'CPU usage: {cpu_percent}%'
+			f'Memory usage: {memory.percent}%'
+			f'Disk usage: {disk.percent}%'
+			f'Network incoming traffic: {net.bytes_recv}'
+		)
 		#logging
-		logging.info(f'CPU usage: {cpu_percent}%')
-		logging.info(f'Memory usage: {memory.percent}%')
-		logging.info(f'Disk usage: {disk.percent}%')
-		logging.info(f'Network incoming traffic: {net.bytes_recv}')
+		logging.info(log_message)
 
-		# logtime
+		#Sending email every 10th time (10 minutes)
+		if int(time.time()) % 60 < 60:
+			send_email('System data logs', log_message)
+
 		time.sleep(60)
+		
 
 if __name__== '__main__':
 	log_system_usage()
